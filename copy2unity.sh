beb6ed282ddb609720a9fccb1fcd9a329d5002c3
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-07-31 05:53:27 +0900

    [fix]: sh

--- copy2unity.sh
+++ copy2unity.sh
@@ -3,8 +3,8 @@
 # if pwd is ~/src/blender/N3D, copy textures and fbx to ~/src/unity/N3D-Unity/Assets/Models folder
 if [ "$PWD" = "/home/$(whoami)/src/blender/N3D" ]; then
     echo "copying files to unity project"
-    cp -i N3D/n3d.fbx ~/src/unity/N3D-Unity/Assets/Models/N3D/
-    cp -ri N3D/textures ~/src/unity/N3D-Unity/Assets/Models/N3D/
+    cp -i ./n3d.fbx ~/src/unity/N3D-Unity/Assets/Models/N3D/
+    cp -ri ./textures ~/src/unity/N3D-Unity/Assets/Models/N3D/
     echo "done"
 else
     echo "please run this script in the blender project root"
