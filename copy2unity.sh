599c5bd6ad824a147460ddfd1cf736892a5dde9c
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-07-30 23:22:20 +0900

    [add]: shell script for exporting to unity

---
+++ copy2unity.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# if pwd is ~/src/blender/N3D, copy textures and fbx to ~/src/unity/N3D-Unity/Assets/Models folder
+if [ "$PWD" = "/home/$(whoami)/src/blender/N3D" ]; then
+    echo "copying files to unity project"
+    cp -i N3D/n3d.fbx ~/src/unity/N3D-Unity/Assets/Models/N3D/
+    cp -ri N3D/textures ~/src/unity/N3D-Unity/Assets/Models/N3D/
+    echo "done"
+else
+    echo "please run this script in the blender project root"
+fi
+
